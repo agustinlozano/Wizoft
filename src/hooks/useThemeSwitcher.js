236cfd0ab4428ffc3856be0cf37b7d66ae4b8072
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2022-07-23 20:51:47 -0300

    Add the first custom hook to switch theme

---
+++ src/hooks/useThemeSwitcher.js
@@ -0,0 +1,14 @@
+import { useEffect, useState } from 'react'
+
+export const useThemeSwitcher = () => {
+  const [isNight, setIsNight] = useState(true)
+
+  useEffect(() => {
+    const isLight = window.matchMedia('(prefers-color-scheme: light)').matches
+    if (isLight) {
+      setIsNight(false)
+    }
+  })
+
+  return { isNight }
+}
