e4dd28c44c134d1ed5ee9c24525540e8ca9724a7
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2022-07-23 20:54:16 -0300

    Add a theme provider using react contex

---
+++ src/contex/themeProvider.jsx
@@ -0,0 +1,20 @@
+import { createContext, useState } from 'react'
+import LightModeTheme from '../styles/LightModeTheme.jsx'
+import DarkModeTheme from '../styles/DarkModeTheme.jsx'
+import { useThemeSwitcher } from '../hooks/useThemeSwitcher.js'
+
+export const ThemeContext = createContext({ error: 'Not permission' })
+
+const ThemeProvider = ({ children }) => {
+  const { isNight } = useThemeSwitcher()
+  const [isDarkMode, setIsDarkMode] = useState(isNight)
+
+  return (
+    <ThemeContext.Provider value={{ isDarkMode, setIsDarkMode }}>
+      {children}
+      {isDarkMode ? <DarkModeTheme /> : <LightModeTheme />}
+    </ThemeContext.Provider>
+  )
+}
+
+export default ThemeProvider
