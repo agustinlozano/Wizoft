60475654cb31f9d3f952e01d4dc5027c09f31828
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2022-07-25 18:21:08 -0300

    Add style settings from theme values

---
+++ src/config/styles.js
@@ -0,0 +1,34 @@
+/**
+ * Utilizo un objeto JavaScript donde guardo
+ * todos los valores que necesito para que el
+ * frontend sea más consistente y fácil de
+ * manejar. Estos estilos dependen de los valores
+ * declarados en el theme.
+ */
+
+export const styles = {
+  /* colors */
+  background: 'var(--background)',
+  veryLowAccent: 'var(--very-low-accent)',
+  lowAccent: 'var(--low-accent)',
+  mediumAccent: 'var(--medium-accent)',
+  hightAccent: 'var(--hight-accent)',
+  darkAccent: 'var(--dark-accent)',
+  transparentBackground: 'var(--transparent-background)',
+
+  /* shadow */
+  lowShadow: 'var(--low-shadow)',
+  mediumShadow: 'var(--medium-shadow)',
+
+  /* media querys */
+  maxButtonWidth: '320px',
+  desktopQuery: '950px',
+  headerMaxWidth: '1024px',
+  LayoutMaxWidth: '1440px',
+
+  /* fonts */
+  fontRegular: 'var(--font-regular)',
+  fontLight: 'var(--font-light)',
+  fontMedium: 'var(--font-medium)',
+  fontBlack: 'var(--font-black)'
+}
