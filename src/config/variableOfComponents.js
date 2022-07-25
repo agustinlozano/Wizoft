be6b7c2b34fb6d3db8f5e8845767f8353e61d09c
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2022-07-25 18:51:24 -0300

    Add a new config file

---
+++ src/config/variableOfComponents.js
@@ -0,0 +1,23 @@
+export const BUTTONS_TYPES = {
+  menu: -1,
+  primary: 0,
+  secondary: 1,
+  icon: 2
+}
+
+export const TEXT_ALIGN = {
+  left: -1,
+  center: 0,
+  rigth: 1
+}
+
+export const TITLE_TYPES = {
+  primary: -1,
+  secondary: 0,
+  tertiary: 1
+}
+
+export const TEXT_TYPES = {
+  low: -1,
+  normal: 0
+}
