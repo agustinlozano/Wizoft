aafce74805d947a3a8150239306b61779f3124ad
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2022-07-25 18:52:10 -0300

    Add a new config file with components information

---
+++ src/config/globalLinks.js
@@ -0,0 +1,71 @@
+import IconDownload from 'components/ui/IconDownload'
+import IconGitHub from 'components/ui/IconGitHub'
+import IconLinkedIn from 'components/ui/IconLinkedIn'
+import IconMail from 'components/ui/IconMail'
+
+import { BUTTONS_TYPES } from './variableOfComponents'
+
+export const linksToToggleMenu = [
+  {
+    title: 'Inicio',
+    url: '/',
+    icon: null
+  },
+  {
+    title: 'Mi Historia',
+    url: '/about-me',
+    icon: null
+  },
+  {
+    title: 'Diseño UI',
+    url: '/ui-design',
+    icon: null
+  },
+  {
+    title: 'Proyectos',
+    url: '/projects',
+    icon: null
+  },
+  {
+    title: 'Formas de contacto',
+    url: '/contact',
+    icon: null
+  },
+  {
+    title: 'Curriculum',
+    url: '/curriculum',
+    type: BUTTONS_TYPES.secondary,
+    icon: <IconDownload />
+  }
+]
+
+export const socialMediaLinks = [
+  {
+    title: 'GitHub',
+    url: 'https://github.com/agustinlozano',
+    icon: <IconGitHub />
+  },
+  {
+    title: 'LinkedIn',
+    url: 'https://www.linkedin.com/in/agustin-lozano-blua/',
+    icon: <IconLinkedIn />
+  }
+]
+
+export const contactLinks = [
+  {
+    title: 'Visitar mi LinkedIn',
+    url: 'https://www.linkedin.com/in/agustin-lozano-blua/',
+    icon: <IconLinkedIn />
+  },
+  {
+    title: 'Visitar mi GitHub',
+    url: 'https://github.com/agustinlozano',
+    icon: <IconGitHub />
+  },
+  {
+    title: 'Hablar por mail',
+    url: 'mailto:agustinlozanoblua@gmail.com',
+    icon: <IconMail />
+  }
+]
