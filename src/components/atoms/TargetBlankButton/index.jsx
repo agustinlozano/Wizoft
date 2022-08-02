5b7114c603e2102946be58545bc1a8b4c6694383
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2022-08-02 19:10:36 -0300

    Add a new atom component

---
+++ src/components/atoms/TargetBlankButton/index.jsx
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types'
+import { Icon, Link } from './styles'
+
+const TargetBlankButton = ({ type, url, icon }) => {
+  return (
+    <Link target='_blank' rel='noreferrer' type={type} href={url}>
+      {icon && <Icon type={type}>{icon}</Icon>}
+    </Link>
+  )
+}
+
+TargetBlankButton.propTypes = {
+  title: PropTypes.string,
+  type: PropTypes.oneOfType([PropTypes.number, PropTypes.any]),
+  url: PropTypes.string.isRequired,
+  icon: PropTypes.oneOfType([PropTypes.any, PropTypes.node])
+}
+
+export default TargetBlankButton
