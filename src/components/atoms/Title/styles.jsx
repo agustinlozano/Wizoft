398410b5130256c9ba12e1577bd85640aab8ee04
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2022-08-01 23:00:13 -0300

    Add a new atom component: Title

---
+++ src/components/atoms/Title/styles.jsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled'
+import { styles } from '../../../config/styles'
+import { handleSelectStylesFromType } from './selectTitleStylesFromType'
+
+export const Text = styled.h1`
+  color: ${styles.darkAccent};
+  font-weight: 400;
+  margin-top: 0;
+  margin-bottom: 0;
+  ${({ type }) => handleSelectStylesFromType(type)}
+`
