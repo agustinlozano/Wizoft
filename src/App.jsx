5b095e10fd786f3e03947a02a4a5c18f25bf53da
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2022-08-02 21:32:04 -0300

    Update App according to the changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import ThemeProvider from './context/themeProvider'
 import PageRouters from './routes'
 import './App.css'
+import Header from './components/layouts/Header'
 
 function App () {
   return (
     <ThemeProvider>
       <div className='App'>
+        <Header />
         <PageRouters />
       </div>
     </ThemeProvider>
