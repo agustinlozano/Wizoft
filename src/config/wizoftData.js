0aa803c651c6d1f9de1057576bdd5cbbba57dc43
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2022-08-07 21:48:24 -0300

    Add product stage data to wizard data

---
+++ src/config/wizoftData.js
@@ -0,0 +1,32 @@
+export const producStages = [
+  {
+    title: 'Requisitos del cliente',
+    subTitle: 'Captar la necesidad del cliente',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, asperiores ratione odit doloribus delectus accusantium iste autem obcaecati porro a, vel in, possimus quibusdam. Rem aspernatur alias hic exercitationem vitae!'
+  },
+  {
+    title: 'Análisis del proyecto',
+    subTitle: 'Captar la necesidad del cliente',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, asperiores ratione odit doloribus delectus accusantium iste autem obcaecati porro a, vel in, possimus quibusdam. Rem aspernatur alias hic exercitationem vitae!'
+  },
+  {
+    title: 'Diseño & estética',
+    subTitle: 'Captar la necesidad del cliente',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, asperiores ratione odit doloribus delectus accusantium iste autem obcaecati porro a, vel in, possimus quibusdam. Rem aspernatur alias hic exercitationem vitae!'
+  },
+  {
+    title: 'Desarrollo del producto',
+    subTitle: 'Captar la necesidad del cliente',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, asperiores ratione odit doloribus delectus accusantium iste autem obcaecati porro a, vel in, possimus quibusdam. Rem aspernatur alias hic exercitationem vitae!'
+  },
+  {
+    title: 'Testing & QA',
+    subTitle: 'Captar la necesidad del cliente',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, asperiores ratione odit doloribus delectus accusantium iste autem obcaecati porro a, vel in, possimus quibusdam. Rem aspernatur alias hic exercitationem vitae!'
+  },
+  {
+    title: 'Desplegar a producción',
+    subTitle: 'Captar la necesidad del cliente',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, asperiores ratione odit doloribus delectus accusantium iste autem obcaecati porro a, vel in, possimus quibusdam. Rem aspernatur alias hic exercitationem vitae!'
+  }
+]
