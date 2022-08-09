c3f7a6a1c5f0695f96b2e1c37481b7bea01be324
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2022-08-08 21:04:02 -0300

    Add media queries for device view

--- src/components/atoms/Logo/styles.jsx
+++ src/components/atoms/Logo/styles.jsx
@@ -1,5 +1,16 @@
 import styled from '@emotion/styled'
+import { styles } from '../../../config/styles'
 
 export const LinkTo = styled.a`
   display: block;
+  width: 10rem;
+
+  @media (min-width: ${styles.desktopQuery}) {
+    width: 15rem;
+  }
+
+  @media (max-width: ${styles.mobileQuery}) {
+    display: block;
+    margin: auto;
+  }
 `
